a06a2936a9c7fe5fce7707ca16e1efdc4c57c0cb
krisnokumarghosh <krishnokumarghosh112@gmail.com>
2026-05-20 03:06:48 +0600

    connected mongodb

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const express = require("express");
-const app = express();
+const dotenv = require("dotenv");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+dotenv.config();
 
-const PORT = 8000;
+const uri = process.env.MONGODB_URI;
+
+const app = express();
+const PORT = process.env.PORT;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    const db = client.db("medzy");
+    const allDoctorCollection = db.collection("all-doctor");
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    await client.close();
+  }
+};
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server is running fine!!!");
